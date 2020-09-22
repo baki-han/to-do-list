8296668b7da15c1dd177ebcc674501959181653f
baki-han <baki47hanma@gmail.com>
2020-09-22 13:56:36 -0500

    just a test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Header from "./components/Header";
+import List from "./components/List";
 
 function App() {
-  return (
+
+    const [notes, setNotes] = useState([]);
+
+    const addNote = (value)=>{
+      let newList = [...notes];
+      newList.push(value);
+      setNotes(newList);
+    }
+
+    const removeItem = (index)=>{
+        setNotes(notes.filter((el, i)=> index !== i));
+    }
+
+    /*  <<< THIS IS A FUNCTION TO RESET BACK TO DEFAULT >>> //
+    const resetList = ()=>{
+       setNotes([])
+    } */
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header onClick={addNote} />
+      <hr/>
+      <List notes={notes} removeItem={removeItem} />
     </div>
   );
 }
