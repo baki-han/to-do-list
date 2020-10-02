380175e644a05629567948ba4604b352997d7dde
baki-han <baki47hanma@gmail.com>
2020-10-01 23:23:03 -0500

    deploy todo

---
+++ build/precache-manifest.b4e7b4200b673691078479d2c6dac9fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e2ad4b41d6ab37b45782e6ed85992c",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "a6d6dd54d62a8068bbab",
+    "url": "/to-do-list/static/css/main.fba15f40.chunk.css"
+  },
+  {
+    "revision": "8229f98cb55524b012cd",
+    "url": "/to-do-list/static/js/2.ff0e2dff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.ff0e2dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6d6dd54d62a8068bbab",
+    "url": "/to-do-list/static/js/main.0f7a340c.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  }
+]);
